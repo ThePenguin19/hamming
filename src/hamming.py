34ed213db831cfa63c0f2a2025118445fce81842
Connor <ConnorBDaily@Gmail.com>
2025-12-04 16:01:43 -0800

    sollution

--- src/hamming.py
+++ src/hamming.py
@@ -1,2 +1,13 @@
-def hamming_distance(strand_1, strand_2):
-    pass
+def hamming_distance(list1, list2):
+    diffrences = 0
+    if len(list1) != len(list2):
+        return None
+        
+    for i in range(len(list1)):
+        if list1[i] not in "GCAT":
+            return None
+        if list1[i] != list2[i]:
+            diffrences += 1
+
+    return diffrences
+
